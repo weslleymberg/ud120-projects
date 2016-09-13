16885988196676ac48f3a0504e7b2265c1166037
weslleymberg <weslleym.lisboa@gmail.com>
2016-09-13 16:42:14 -0300

    Solved Naive Bayes timing quiz

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -34,7 +34,14 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 
 clf = GaussianNB()
+
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
 pred_labels = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
 accuracy = accuracy_score(labels_test, pred_labels)
 print accuracy
