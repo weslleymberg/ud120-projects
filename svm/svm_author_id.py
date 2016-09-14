f65c63f7b9047ff033200db00247b4ea7fa38f4f
weslleymberg <weslleym.lisboa@gmail.com>
2016-09-14 14:07:39 -0300

    Solved SVM timing quiz

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -31,8 +31,14 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 clf = SVC(kernel='linear')
+
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
 pred_labels = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
 
 accuracy = accuracy_score(labels_test, pred_labels)
 print accuracy
