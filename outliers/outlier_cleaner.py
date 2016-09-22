d7be406a96cf0fa0b7afd16bb47d095963c1a1fe
weslleymberg <weslleym.lisboa@gmail.com>
2016-09-22 16:50:06 -0300

    Solved outliers first mini-project

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,21 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+    grouped_data = zip(ages, net_worths, predictions)
 
-    
+    calculated_error = [(age, net_worth, (net_worth-prediction)**2) for age, net_worth, prediction in grouped_data]
+
+    calculated_error.sort(key=lambda (x,y,error): error)
+
+    cleaned_data = calculated_error[:int(len(calculated_error)*0.9)]
+    print len(cleaned_data)
+
     return cleaned_data
 
